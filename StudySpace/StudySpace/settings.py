22ae08f617ab92e480d2fe7e063b890c96692aa7
macktvz <macktvz@gmail.com>
2021-04-09 22:11:08 -0400

    req

--- StudySpace/StudySpace/settings.py
+++ StudySpace/StudySpace/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -159,3 +160,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+
+django_heroku.settings(locals())

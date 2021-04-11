7c7bf28b9059a27269afbaa01efe7f2ef8e1af81
macktvz <macktvz@gmail.com>
2021-04-10 21:37:51 -0400

    fixed picture function

--- StudySpaceApi/views.py
+++ StudySpaceApi/views.py
@@ -5,6 +5,7 @@ from rest_framework.response import Response
 from rest_framework import viewsets
 from rest_framework.decorators import action
 from itertools import chain
+from django.utils.html import mark_safe
 # Create your views here.
 
 
